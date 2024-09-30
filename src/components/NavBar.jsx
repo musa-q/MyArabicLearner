398c93b9ff4ca44d64182beb836e1f1a9e672b3e
Musa Qureshi <musaqureshi2002@gmail.com>
2024-09-30 22:34:05 +0100

    Fix closing NavBar on click

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,8 +6,15 @@ import logo from '/logo_main.svg';
 import './NavBar.css';
 
 const MyNavBar = ({ onNavigate }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleNavigate = (path) => {
+        onNavigate(path);
+        setExpanded(false);
+    };
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-tertiary" expanded={expanded}>
             <Container>
                 <Navbar.Brand onClick={() => onNavigate('home')}>
                     <div className='aref-ruqaa-regular gold nav-title logo-container unselectable'>
@@ -14,13 +22,13 @@ const MyNavBar = ({ onNavigate }) => {
                         متعلمو العربية
                     </div>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link onClick={() => onNavigate('home')}>Home</Nav.Link>
-                        <Nav.Link onClick={() => onNavigate('wordsflashcard')}>Flashcards</Nav.Link>
-                        <Nav.Link onClick={() => onNavigate('wordspractice')}>Vocab Quiz</Nav.Link>
-                        <Nav.Link onClick={() => onNavigate('verbs')}>Conjugation Quiz</Nav.Link>
+                        <Nav.Link onClick={() => handleNavigate('home')}>Home</Nav.Link>
+                        <Nav.Link onClick={() => handleNavigate('wordsflashcard')}>Flashcards</Nav.Link>
+                        <Nav.Link onClick={() => handleNavigate('wordspractice')}>Vocab Quiz</Nav.Link>
+                        <Nav.Link onClick={() => handleNavigate('verbs')}>Conjugation Quiz</Nav.Link>
                     </Nav>
                     <div className="d-flex align-items-center">
                         <span className="me-3">Follow the developer:</span>
