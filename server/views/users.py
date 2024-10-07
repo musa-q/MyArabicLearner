17b5a07c7646cb2b03eb3dde658261100d829dac
Musa Qureshi <musaqureshi2002@gmail.com>
2024-10-07 21:22:50 +0100

    Add delete user route

--- server/views/users.py
+++ server/views/users.py
@@ -24,3 +24,29 @@ def view_users(*args, **kwargs):
     users = User.query.all()
     user_list = [{'id': cat.id, 'username': cat.username, 'email': cat.email} for cat in users]
     return jsonify(user_list), 200
+
+@users_bp.route('/delete-user', methods=['DELETE'])
+@require_auth(allowed_roles=['admin'])
+def delete_user(*args, **kwargs):
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+
+    if not username and not email:
+        return jsonify({'error': 'Email or username is required'}), 400
+
+    if username:
+        user = User.query.filter_by(username=username).first()
+    elif email:
+        user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    try:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': f'User {username} deleted successfully'}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': f'An error occurred while deleting the user: {str(e)}'}), 500
